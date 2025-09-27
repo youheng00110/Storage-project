fce913389322a528bfe34ccc39598ff722e03793
youheng00110 <3037486989@qq.com>
2025-09-27 11:11:01 +0800

    My project/python learning/001.py

---
+++ My project/Class.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    float f,i;
+scanf("%f,%f",&i,&f);
+printf("i=%f,f=%f",i,f);
+return 0;
+}
---
+++ My project/python learning/001.py
@@ -0,0 +1,4 @@
+salaryperhour=(int(input("Enter your salary per hour: ")))
+workinghours=(int(input("Enter your working hours: ")))
+salary=sa25laryperhour*workinghours
+print("your total salary is:",salary)
