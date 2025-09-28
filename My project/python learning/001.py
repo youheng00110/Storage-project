16993c95713439be733baacb5ae934b503ec4e4f
youheng00110 <3037486989@qq.com>
2025-09-28 16:56:07 +0800

    pythonProject/猜数字.py

--- My project/python learning/001.py
+++ My project/python learning/001.py
@@ -1,4 +1,7 @@
-salaryperhour=(int(input("Enter your salary per hour: ")))
-workinghours=(int(input("Enter your working hours: ")))
+try:
+    salaryperhour=(int(input("Enter your salary per hour: ")))
+    workinghours=(int(input("Enter your working hours: ")))
+     except :
+    print("Invalid input, please enter a number.")
 salary=sa25laryperhour*workinghours
 print("your total salary is:",salary)
