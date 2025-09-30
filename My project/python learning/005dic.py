dd61ecf30e71a2a6f7b8daedfa48bb56efd150ea
youheng00110 <3037486989@qq.com>
2025-09-30 22:24:27 +0800

    test.txtMy project/python learning/005dic.py

--- My project/python learning/005dic.py
+++ My project/python learning/005dic.py
@@ -1,6 +1,14 @@
+file=input("请输入文件名：")
+handle=open(file)
 counts=dict()
-lines=input("请输入一段文字：")
-words=lines.split()
-for word in words:
-    counts[word]=counts.get(word,0)+1
-print("各单词出现的次数为：",counts)
+for lines in handle:
+    words=lines.lstrip().split()
+    for word in words:
+        counts[word]=counts.get(word,0)+1
+bigword=None
+bigcount=None
+for word,count in counts.items():
+    if bigcount is None or count>bigcount:
+        bigcount=count
+        bigword=word
+print("各单词出现的次数为：",counts,"\n","最多的单词是：",bigword,"\n出现",bigcount,"次")
