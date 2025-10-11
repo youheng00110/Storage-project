fe4cf0ea2cb202a18e43a31b685f93be7cfb5b19
youheng00110 <3037486989@qq.com>
2025-10-11 15:16:26 +0800

    My project/C learning/002scanf.cMy project/C learning/output/002scanf.exe

---
+++ My project/C learning/002scanf.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(void){
+    /*1.2
+    char name[100];
+    int age;
+    printf("请输入你的名字和年龄：");
+    scanf("%s%d",name,&age);
+    printf("你好呀%d岁的%s，很高兴见到你",age,name);
+    return 0;*/
+    
+    
+    
+    /*1.3
+    int a,b,sum,dif,mul;
+    float div;
+    printf("请输入两个整数，用空格分隔：");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    dif=a-b;
+    mul=a*b;
+    div=(float)a/b;
+    printf("和：%d\n差：%d\n积：%d\n商：%.2f\n",sum,dif,mul,div);
+    return 0;*/
+    
+    
+    /*1.4
+    int a,b,c;
+    printf("请输入想要交换的两个整数");
+    scanf("%d %d", &a, &b);
+    printf("交换前的值为：%d %d\n",a,b);
+    c=a;
+    a=b;
+    b=c;
+    printf("交换后的值为：%d %d",a,b);
+    return 0;*/
+    
+}
